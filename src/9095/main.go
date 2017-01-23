0c69c74d2e96caaa26b92d869e7e71da66a206ab
sapphire <sapphiredev@users.noreply.github.com>
2017-01-24 05:15:00 +0900

    commit

--- src/9095/main.go
+++ src/9095/main.go
@@ -1,14 +1,14 @@
 package main
 
-package main
-
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-    var a int
-    fmt.Scanf("%d", &a)
-    fmt.Printf("%d\n", a+b)
+    var a int;
+    fmt.Scanf("%d", &a);
+
+		for range make([]int, a) {
+			fmt.Println(1);
+		}
 }

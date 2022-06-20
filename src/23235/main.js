7a2b95aef089f01a667c80a53b55005f0aacb00b
sapphire <info@sapphire.sh>
2022-06-20 11:47:08 +0900

    commit

---
+++ src/23235/main.js
@@ -0,0 +1,21 @@
+(() => {
+	const readline = require('readline');
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+	const buffer = [];
+	rl.on('line', (line) => {
+		buffer.push(line);
+	});
+	rl.on('close', () => {
+		const input = buffer;
+		main(input);
+	});
+})();
+
+const main = (input) => {
+	input.pop();
+	const p = input.map((_, i) => `Case ${i + 1}: Sorting... done!`).join('\n');
+	console.log(p);
+};

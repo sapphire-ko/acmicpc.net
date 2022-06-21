218535c45f96c594a7acc018f50021d7e1e34c00
sapphire <info@sapphire.sh>
2022-06-21 23:28:21 +0900

    commit

---
+++ src/23375/main.js
@@ -0,0 +1,26 @@
+(() => {
+	const readline = require('readline');
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+	const buffer = [];
+	rl.on('line', (line) => {
+		buffer.push(line);
+	});
+	rl.on('close', () => {
+		const input = buffer.map((x) => x.split(' ').map(Number));
+		main(input);
+	});
+})();
+
+const main = ([[x, y], [r]]) => {
+	const p = r;
+	const q = [
+		[x - p, y - p],
+		[x - p, y + p],
+		[x + p, y + p],
+		[x + p, y - p],
+	];
+	console.log(q.map((x) => x.join(' ')).join('\n'));
+};

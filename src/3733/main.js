8325959c27eba4dc28dbfdd3c93f15478918486b
sapphire <info@sapphire.sh>
2022-07-22 23:47:43 +0900

    #3733

---
+++ src/3733/main.js
@@ -0,0 +1,20 @@
+(() => {
+	const readline = require('readline');
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+	const buffer = [];
+	rl.on('line', (line) => {
+		buffer.push(line);
+	});
+	rl.on('close', () => {
+		const input = buffer.map((x) => x.split(' ').map(Number));
+		main(input);
+	});
+})();
+
+const main = (input) => {
+	const p = input.map(([p, q]) => Math.floor(q / (p + 1)));
+	console.log(p.join('\n'));
+};

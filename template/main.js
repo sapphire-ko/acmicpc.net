e71431605482a8e7f95bb2a1ba7e994c5405c4a0
sapphire <info@sapphire.sh>
2022-08-02 22:57:59 +0900

    update template

--- template/main.js
+++ template/main.js
@@ -2,7 +2,6 @@
 	const readline = require('readline');
 	const rl = readline.createInterface({
 		input: process.stdin,
-		output: process.stdout,
 	});
 	const buffer = [];
 	rl.on('line', (line) => {
